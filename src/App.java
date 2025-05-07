7530fd6208e3dac763c7c97a73bf888de50ebd1b
FillipeTomaz <125302557+FillipeTomaz@users.noreply.github.com>
2025-05-07 14:35:04 -0300

    Add files via upload

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main() {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {
+        
     }
+
+
 }
