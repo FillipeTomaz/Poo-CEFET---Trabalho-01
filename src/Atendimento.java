899ab6a4c8268ba3230415173c73b989004815f4
FillipeTomaz <125302557+FillipeTomaz@users.noreply.github.com>
2025-05-12 15:21:30 -0300

    Add files via upload

--- src/Atendimento.java
+++ src/Atendimento.java
@@ -1,3 +1,36 @@
+import java.util.Scanner;
+
 public class Atendimento {
+    Scanner sc = new Scanner(System.in);
+
+    public Veiculo adcioniarVeiculo(String placa, String modelo, String cor, String marca, Veiculo.Tipo tipo) {
+        System.out.println("Digite a placa do veiculo: ");
+        placa = sc.next();
+        System.out.println("Digite o modelo do veiculo: ");
+        modelo = sc.next();
+        System.out.println("Digite a cor do veiculo: ");
+        cor = sc.next();
+        System.out.println("Digite a marca do veiculo: ");
+        marca = sc.next();
+        System.out.println("Digite o tipo do veiculo: ");
+        tipo = Veiculo.Tipo.valueOf(sc.next().toLowerCase());
+        Veiculo veiculo = new Veiculo(placa, modelo, cor, marca, tipo);
+        return veiculo;
+    }
+
+    public void fluxo(){
+        fluxo
+
+
+    }
+
+    public void fluxoEntrada(Veiculo veiculo, Vagas vaga) {
+       
+    }
+
+
+    public void fluxoSaida(Veiculo veiculo, Vagas vaga) {
+        
+    }
     
 }
--- src/Gestão.java
+++ src/Gestão.java
@@ -1,8 +1,40 @@
+import java.util.Scanner;
+
 public class Gestão {
+
+    Scanner sc = new Scanner(System.in);
 
 
     int a = 10;
     Vagas[] Estacionamento = new Vagas[a];
+    float tarifa;
+    float hora;
+
+    public void adicionarVagas() {
+        
+            System.out.println("Digite o número da vaga: ");
+            int numero = sc.nextInt();
+            System.out.println("Digite o tipo do veiculo: ");
+            Veiculo.Tipo tipo = Veiculo.Tipo.valueOf(sc.next().toLowerCase());
+            System.out.println("Digite o estado da vaga: ");
+            boolean ocupado = false;
+            Veiculo veiculo = null;
+
+            
+            Estacionamento[a-1] = new Vagas(numero, tipo, ocupado, veiculo);
+            System.out.println("Vaga adicionada com sucesso!");
+            a++; 
+    }
+
+    public float alterarTarifa(float tarifa) {
+        System.out.println("Valor fixo da tarifa: ");
+        tarifa = sc.nextFloat();
+        System.out.println("Valor adicional da tarifa por hora(após 4 horas): ");
+        hora = sc.nextFloat();
+        System.out.println("Valor da tarifa alterado com sucesso!");
+        return tarifa;
+
+    }
 
     
 
