9e2fe5ce175f37bc62a4088719407748e959adb2
FillipeTomaz <125302557+FillipeTomaz@users.noreply.github.com>
2025-05-19 10:37:53 -0300

    Atualizar o Gestão.java

--- src/Gestão.java
+++ src/Gestão.java
@@ -1,42 +1,67 @@
 import java.util.Scanner;
 
 public class Gestão {
-
     Scanner sc = new Scanner(System.in);
-
-
-    int a = 10;
-    Vagas[] Estacionamento = new Vagas[a];
+    Vagas[] Estacionamento = new Vagas[100];
     float tarifa;
     float hora;
+    private int totalVagas = 0;
+
+    public void menuGestao() {
+        int opcao;
+        do {
+            System.out.println("\n=== Menu Gestão ===");
+            System.out.println("1 - Adicionar vaga");
+            System.out.println("2 - Alterar tarifas");
+            System.out.println("3 - Listar vagas");
+            System.out.println("4 - Voltar");
+            System.out.print("Escolha: ");
+            opcao = sc.nextInt();
+
+            switch(opcao) {
+                case 1:
+                    adicionarVagas();
+                    break;
+                case 2:
+                    alterarTarifa();
+                    break;
+                case 3:
+                    listarVagas();
+                    break;
+            }
+        } while(opcao != 4);
+    }
 
     public void adicionarVagas() {
+        System.out.print("Número da vaga: ");
+        int numero = sc.nextInt();
+        System.out.print("Tipo (moto/carro/utilitário): ");
+        Veiculo.Tipo tipo = Veiculo.Tipo.valueOf(sc.next().toLowerCase());
         
-            System.out.println("Digite o número da vaga: ");
-            int numero = sc.nextInt();
-            System.out.println("Digite o tipo do veiculo: ");
-            Veiculo.Tipo tipo = Veiculo.Tipo.valueOf(sc.next().toLowerCase());
-            System.out.println("Digite o estado da vaga: ");
-            boolean ocupado = false;
-            Veiculo veiculo = null;
-
-            
-            Estacionamento[a-1] = new Vagas(numero, tipo, ocupado, veiculo);
-            System.out.println("Vaga adicionada com sucesso!");
-            a++; 
+        if(totalVagas < Estacionamento.length) {
+            Estacionamento[totalVagas++] = new Vagas(numero, tipo, false, null);
+            System.out.println("Vaga adicionada!");
+        } else {
+            System.out.println("Limite de vagas atingido!");
+        }
     }
 
-    public float alterarTarifa(float tarifa) {
-        System.out.println("Valor fixo da tarifa: ");
+    public void alterarTarifa() {
+        System.out.print("Nova tarifa base: ");
         tarifa = sc.nextFloat();
-        System.out.println("Valor adicional da tarifa por hora(após 4 horas): ");
+        System.out.print("Tarifa por hora adicional: ");
         hora = sc.nextFloat();
-        System.out.println("Valor da tarifa alterado com sucesso!");
-        return tarifa;
-
+        System.out.println("Tarifas atualizadas!");
     }
 
-    
-
-
+    public void listarVagas() {
+        System.out.println("\n=== Vagas ===");
+        for(int i = 0; i < Estacionamento.length; i++) {
+            Vagas v = Estacionamento[i];
+            if(v != null) {
+                String status = v.ocupado ? "Ocupada" : "Livre";
+                System.out.printf("Vaga %d - %s - %s%n", v.numero, v.tipoVaga, status);
+            }
+        }
+    }
 }
